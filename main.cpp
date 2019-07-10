dcc59e7d7c8bfd01039eb3722ef7ee28da498541
Zeke Williams <broncoscorpio@gmail.com>
2019-07-10 16:02:15 -0600

    Check for config file in main

--- main.cpp
+++ main.cpp
@@ -4,16 +4,17 @@ using namespace std;
 
 // automatically tell which type of bureau it is
 string whichBur(const string&);
+string readConfigFile();
 
 int main(int argc, char* argv[]) {
 
-	// if (argc < 2) {
-	//  	cout << "Enter the bureau file you want to edit as the argument!" << endl;
-	// 	return 1;
-	// }
+	if (argc < 2) {
+	 	cout << "Enter the bureau file you want to edit as the argument!" << endl;
+	 	return 1;
+	}
 	
 	// pass the argument into a string (might want for later)
-	string burFile = "../B123MYRX.3JJ";//argv[1];
+	string burFile = argv[1];//"./B123MYRX.3Lo";//argv[1]
 	
 	string bureau = whichBur(burFile);
 	cout << "Bureau is " << bureau << endl;
@@ -21,8 +22,7 @@ int main(int argc, char* argv[]) {
 	if (bureau == "Error")
 		return 2;
 
-	string dbfPath = "../";
-
+	string dbfPath = readConfigFile();
 
 	DBF dbf(dbfPath, bureau);
 	
@@ -30,7 +30,6 @@ int main(int argc, char* argv[]) {
 	
 	dbf.editBureauFile();
 
-	//std::cout << "Make it here?" << std::endl;
 	return 0;
 }
 
@@ -57,4 +56,51 @@ string whichBur(const string& burFilePath) {
 		cout << "Could not open \"" << burFilePath << "\"." << endl;
 		return "Error";
 	}
+}
+
+string readConfigFile() {
+	// default path location
+	string defaultPath = "/home/ewilliams/cvs/wfsp/crit/";
+	
+	// get current user's name to create the config file in their home directory
+	system("echo $USERNAME > usertmp.txt");
+	
+	ifstream userIn("usertmp.txt");
+	if (userIn.is_open()) {
+		string ln;
+		std::getline(userIn, ln);
+		userIn.close();
+		cout << "Username obtained is: " << ln << endl;
+		// create a path string to the user's home directory
+		string configFile = "/home/" + ln + "/.burEdCfg";
+	
+		// remove it once the username has been obtained.
+		system("rm -f usertmp.txt");
+		
+		// attempt to open an existing config file for reading. If that fails, they don't have one, so create a config file
+		ifstream cfgIn(configFile.c_str());
+		if (!cfgIn.is_open()) {
+			cout << "Creating config file in your home directory: \"" << configFile << "\"" << endl;
+			ofstream cfgOut(configFile.c_str());
+			cfgOut << defaultPath;
+			cfgOut.close();
+		}
+		else {
+			// if user had a config file, read the path they want for their parse-map files and return it
+			cout << "A config file exists. Reading preferred path to parse-map file...";
+			string path = "";
+			std:getline(cfgIn, path);
+			cfgIn.close();
+			
+			if (path == "")
+				path = defaultPath;
+			
+			cout << "\"" << path << "\"" << endl;
+		}
+	}
+	else {
+		cout << "Could not open temporary user file. Permissions to create it possibly have something to do with it??" << endl;
+	}
+	
+	return defaultPath;
 }
