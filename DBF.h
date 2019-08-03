461ecb049fada47f9c0f1b5cd9a73a059eb0093c
Ezekiel Williams <broncoscorpio@gmail.com>
2019-08-03 08:53:00 -0600

    Add DBF.h remaining conflict changes.

--- DBF.h
+++ DBF.h
@@ -33,18 +33,11 @@ private:
 	std::string editSeg;
 	std::string timeTag;
 	std::string tmpFileName;
-<<<<<<< HEAD
 	std::string optArg;
 		
 	bool optionView;
 		
 	std::ofstream errLog;
-||||||| merged common ancestors
-=======
-	std::string optArg;
-
-	bool optionView;
->>>>>>> feat
 
 public:
 	DBF() {};
